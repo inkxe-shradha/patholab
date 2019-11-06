bdab49b6015c97affa0a083d3018f2a593aa4470
inkxe-shradha <shradhap@riaxe.com>
2019-11-06 20:54:26 +0530

    Sql changes

--- schema/patho_lab.sql
+++ schema/patho_lab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2019 at 08:03 PM
+-- Generation Time: Nov 06, 2019 at 04:21 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -25,6 +25,69 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `patient_data`
+--
+
+CREATE TABLE `patient_data` (
+  `id` int(11) NOT NULL,
+  `patient_name` varchar(50) NOT NULL,
+  `patient_number` bigint(20) DEFAULT NULL,
+  `patient_age` int(11) DEFAULT NULL,
+  `patient_gender` enum('male','female') DEFAULT NULL,
+  `address` text NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relation_report`
+--
+
+CREATE TABLE `relation_report` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `report_name` varchar(200) NOT NULL,
+  `report_price` float(20,2) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_data`
+--
+
+CREATE TABLE `report_data` (
+  `id` int(11) NOT NULL,
+  `test_name` varchar(250) NOT NULL,
+  `test_price` float(20,2) DEFAULT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `text_details_report`
+--
+
+CREATE TABLE `text_details_report` (
+  `id` int(11) NOT NULL,
+  `patient_id` int(11) NOT NULL,
+  `report_id` int(11) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `age` varchar(20) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `address` text,
+  `total_report_price` float(20,2) DEFAULT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -38,15 +101,32 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `user_id`, `username`, `email`, `password`, `created_date`) VALUES
-(1, 4162475, 'shradhasuman2', 'shradhasuman2@gmail.com', '$2y$10$zwBOVUKs.6Wak4pao5GwI.y/dsKbiPBryWviTv7dLLzQ6tYNWbEpS', '2019-09-25 09:14:32');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `patient_data`
+--
+ALTER TABLE `patient_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `relation_report`
+--
+ALTER TABLE `relation_report`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `report_data`
+--
+ALTER TABLE `report_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `text_details_report`
+--
+ALTER TABLE `text_details_report`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -59,10 +139,34 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `patient_data`
+--
+ALTER TABLE `patient_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `relation_report`
+--
+ALTER TABLE `relation_report`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `report_data`
+--
+ALTER TABLE `report_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `text_details_report`
+--
+ALTER TABLE `text_details_report`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
